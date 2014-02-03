3da7af91d024be88c71f8f2f8d04d60b3f16d6b8
Roberto Cunha <robertosilvino@gmail.com>
2014-02-02 23:59:09 -0200

    Finish sign in - default

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
   resources :users
-  #get "users/new"
+  resources :sessions, only: [:new, :create, :destroy]
 
   #match '/', to: 'static_pages#home', via: 'get'
   root  'static_pages#home'
 
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
